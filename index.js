3d1b58e1ee09b202d32e111524ea68adf8a43b05
Jacob Roth <jakeroth0@gmail.com>
2023-01-14 18:24:06 -0500

    testing anhcor tags

--- index.js
+++ index.js
@@ -11,23 +11,66 @@ inquirer
   .prompt([
     {
         type: 'input',
-        message: 'What is your name?',
+        message: 'What is your GitHub username?',
         name: 'userName',
-        default: 'Doof',
+        default: 'GitHub username',
     },
     {
         type: 'input',
-        message: 'What kind of file do you want to make?',
-        name: 'fileType',
-        default: 'a good one',
+        message: 'What is your email address?',
+        name: 'email',
+        default: 'google@gmail.com',
+    },
+    {
+        type: 'input',
+        message: 'What is your projects name?',
+        name: 'title',
+        default: 'Title',
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project',
+        name: 'description',
+        default: 'Description',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        name: 'license',
+        choices: ["MIT", "not MIT","Third Option"],
+        default: [0],
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'instal',
+        default: 'npm i',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'tests',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'usage',
+        default: 'run npm test',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contributing',
+        default: 'make a pr',
     },
   ])
   .then((userAnswers) => {
     // console.log('Success!')
     const addAnswersToMarkDown = markDown.generateMarkdown(userAnswers);
-    console.log(addAnswersToMarkDown);
+    // console.log(addAnswersToMarkDown);
     fs.writeFile('README.md', addAnswersToMarkDown, (err) =>
-  err ? console.error(err) : console.log('Success!')
+  err ? console.error(err) : console.log('Generating README...')
 );
     return userAnswers;
     // Use user feedback for... whatever!!
